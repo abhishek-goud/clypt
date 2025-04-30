859c96ab376b01d50d96b99a9563e7ad64895d75
abhishek-goud <122163601+abhishek-goud@users.noreply.github.com>
2025-04-30 22:17:31 +0530

    basic api setup

--- clypt-backend/src/main/java/com/clypt/clypt_backend/controller/AnonymousFileHandlerController.java
+++ clypt-backend/src/main/java/com/clypt/clypt_backend/controller/AnonymousFileHandlerController.java
@@ -1,6 +1,16 @@
 package com.clypt.clypt_backend.controller;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,6 +23,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/clypt/anonymous")
 public class AnonymousFileHandlerController {
+	
+	@PostMapping
+	public ResponseEntity<Object> uploadFile() {
+	    Map<String, Object> response = new HashMap<>();
+	    response.put("message", "File uploaded successfully.");
+	    response.put("timestamp", LocalDateTime.now());
+
+	    return ResponseEntity.status(HttpStatus.SC_CREATED).body(response);
+	}
+
+	
+	@GetMapping
+	public ResponseEntity<Object> getFile(@RequestParam("code") String uniqueCode){
+		return ResponseEntity.status(HttpStatus.SC_ACCEPTED).body(uniqueCode);
+		
+	}
+
+
 
    
 
